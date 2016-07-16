c5e23dac3bd370ca52ae4d15849b571f8892213e
Hirofumi Wakasugi <baenej@gmail.com>
2016-07-16 21:40:17 +0900

    Initial commit

---
+++ spec/rails-test-runner-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import RailsTestRunnerView from '../lib/rails-test-runner-view';
+
+describe('RailsTestRunnerView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
